f9b36339859e2b587a5a4a29b701c1fed055c741
Fredrik93 <fredrikullman93@gmail.com>
2025-08-12 06:47:26 +0200

    make He wo test pass

--- hello.go
+++ hello.go
@@ -1,20 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode"
+)
 
 func ReverseWordsPreserveStructure(input string) string {
 	// convert string to char arr
 	runes := []rune(input)
+	stack := []rune{}
+	var result []rune = []rune{}
 	// iterate through the runes and reverse each word
 	for i := 0; i < len(runes); i++ {
-		fmt.Printf("rune: %c\n", runes[i])
-	}
-	// reverse each word while preserving spaces and punctuation
-	// return the modified string
+		var symbol rune = runes[i];
+		fmt.Printf("symbol (character): %c\n", symbol)
+		if unicode.IsLetter(runes[i]) || unicode.IsDigit(runes[i]) {
+			stack = append(stack, runes[i])
 
-	return input
+		} else if unicode.IsSpace(runes[i]) {
+
+			for len(stack) > 0 {
+
+				top := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				result = append(result, top)
+			}
+			result = append(result, runes[i]) // add the space or punctuation
+
+		}
+
+	}
+	for len(stack) > 0 {
+
+				top := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				result = append(result, top)
+			}
+
+	return string(result)
 }
 
 func main() {
-	fmt.Println("Hello, World!")
 }
